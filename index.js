b27359ab918071643099db620fe23b1606d63990
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-09-06 08:57:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function produceDrivingRange(blockRange) {
   return function(start,end) {
     let startNumber = start.substring(0,start.indexOf('t'));
     let endNumber   = end.substring(0,end.indexOf('t'));
-    let result = endNumber - startNumber + blockRange;
+    let result = endNumber - (startNumber*1 + blockRange);
     
     if(result >= 0) {
       console.log(`within range by ${result}`);
