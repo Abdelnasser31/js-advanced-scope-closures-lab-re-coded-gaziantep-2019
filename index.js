ae8e5a2d46f45604b1877914668754ce8a083309
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-09-06 08:51:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function produceDrivingRange(range) {
+function produceDrivingRange(blockRange) {
   
   return function(start,end) {
     let startNumber = start.substring(0,start.indexOf('t'));
     let endNumber   = end.substring(0,end.indexOf('t'));
-    let result = endNumber - startNumber + range;
+    let result = endNumber - startNumber + blockRange;
     
     if(result >= 0) {
       console.log(`within range by ${result}`);
